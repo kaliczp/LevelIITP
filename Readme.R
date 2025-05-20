f9d00f1167b18f77b073948274837479305a4447
Kalicz Péter <kaliczp@gmail.com>
2025-05-20 19:55:08 +0200

    Skip station names, generate date, test plot

--- Readme.R
+++ Readme.R
@@ -1,4 +1,10 @@
 ## Import
 library(readxl)
-TP <- as.data.frame(read_excel("Havi T P Level II 2017-2022.xlsx"))
+TP <- as.data.frame(read_excel("Havi T P Level II 2017-2022.xlsx", skip = 1))
 
+## Date
+DateTP <- seq.Date(from = as.Date("2017-01-15"),
+                   to = as.Date("2022-12-15"),
+                   by = "months")
+## Test plot
+plot(DateTP, TP[,4])
