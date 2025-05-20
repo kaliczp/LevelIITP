dda637d735265303e3e7b087800ef9b254040e78
Kalicz Péter <kaliczp@gmail.com>
2025-05-20 20:18:59 +0200

    Reorganise data as a list

--- Readme.R
+++ Readme.R
@@ -6,12 +6,21 @@ TP <- as.data.frame(read_excel("Havi T P Level II 2017-2022.xlsx", skip = 1))
 DateTP <- seq.Date(from = as.Date("2017-01-15"),
                    to = as.Date("2022-12-15"),
                    by = "months")
+
+## Create list
 library(xts)
-M01M03 <- xts(TP[,3:4], DateTP)
-M15M16 <- xts(TP[,7:8], DateTP)
-M17 <- xts(TP[,11:12], DateTP)
-M19 <- xts(TP[,15:16], DateTP)
-M21 <- xts(TP[,19:20], DateTP)
+TPlst <- list()
+TPlst[[1]] <- xts(TP[,3:4], DateTP)
+TPlst[[2]] <- xts(TP[,7:8], DateTP)
+TPlst[[3]] <- xts(TP[,11:12], DateTP)
+TPlst[[4]] <- xts(TP[,15:16], DateTP)
+TPlst[[5]] <- xts(TP[,19:20], DateTP)
+
+for(ttactual in 1:length(TPlst))
+    names(TPlst[[ttactual]]) <- c("P","T")
+
+names(TPlst) <- c("M01, M03", "M15, M16", "M17", "M19", "M21")
 
 ## Test plot
-plot(M01M03)
+ttactual <- 1
+plot(TPlst[[ttactual]][,"T"])
