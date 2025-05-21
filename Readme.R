a9d96b0c6154ba21e605da59a7bf1b582a9ed8af
Kalicz Péter <kaliczp@gmail.com>
2025-05-21 10:00:40 +0200

    Flip T and P
    Temperature now the second draw to prevent hide by precipitation

--- Readme.R
+++ Readme.R
@@ -28,25 +28,27 @@ par(mfrow = c(5,1),
     oma = c(3.1,0,0.5,0)
     )
 for(ttactual in 1:length(TPlst)) {
-plot.zoo(TPlst[[ttactual]][,"T"],
-         col = "#ef8a62",
-         ylim = c(-10,50),
-         xlab = "", ylab = "Temperature",
-         xaxt = "n",
-         xaxs = "i", yaxs = "i",
-         lwd = 3
-         )
-par(new = TRUE)
-plot.zoo(TPlst[[ttactual]][,"P"],
-         type = "h",
-         col = "#67a9cf",
-         ylim = c(240,0),
-         xlab = "", ylab = "",
-         xaxt = "n", yaxt = "n",
-         xaxs = "i", yaxs = "i",
-         lwd = 8
-         )
-axis(4)
+    ## Precipitation
+    plot.zoo(TPlst[[ttactual]][,"P"],
+             type = "h",
+             col = "#67a9cf",
+             ylim = c(240,0),
+             xlab = "", ylab = "",
+             xaxt = "n", yaxt = "n",
+             xaxs = "i", yaxs = "i",
+             lwd = 8
+             )
+    axis(4)
+    par(new = TRUE)
+    ## Temperature
+    plot.zoo(TPlst[[ttactual]][,"T"],
+             col = "#ef8a62",
+             ylim = c(-10,50),
+             xlab = "", ylab = "Temperature",
+             xaxt = "n",
+             xaxs = "i", yaxs = "i",
+             lwd = 3
+             )
 }
 axis.Date(1)
 dev.off()
