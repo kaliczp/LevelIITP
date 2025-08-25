0a03b24eaaac5f6dc9484a9cabaa4b19b7e6b088
Kalicz Péter <kaliczp@gmail.com>
2025-08-25 09:51:53 +0200

    File name updated

--- Readme.R
+++ Readme.R
@@ -1,6 +1,6 @@
 ## Import
 library(readxl)
-TP <- as.data.frame(read_excel("Havi T P Level II 2017-2022.xlsx", skip = 1))
+TP <- as.data.frame(read_excel("Havi T P Level II 2017-2023.xlsx", skip = 1))
 
 ## Date
 DateTP <- seq.Date(from = as.Date("2017-01-15"),
