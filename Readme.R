9620dd7871da2cb0571487fdb209d039a69d3e7a
Kalicz Péter <kaliczp@gmail.com>
2025-08-25 11:13:33 +0200

    Select serif font

--- Readme.R
+++ Readme.R
@@ -26,7 +26,7 @@ for(ttactual in 1:length(TPlst))
 names(TPlst) <- c("M01, M03", "M15, M16", "M17", "M19", "M21"
 )
 ## Test plot
-pdf(width = 11/2.54, height = 11/2.54, pointsize = 10)
+pdf(width = 11/2.54, height = 11/2.54, pointsize = 10, family = "serif")
 par(mfrow = c(5,1),
     mar = c(0,4.1,0,4.1),
     oma = c(2.1,0,0.5,0)
